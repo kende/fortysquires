e648c739d1acd41fd83a580a4dd36a42baa7405b
iseff <iseff@iseff.com>
2010-05-23 19:30:37 -0500

    adding ability to add a new venue with /venues/new

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -13,4 +13,22 @@ class VenuesController < ApplicationController
     end
   end
 
+  def new
+  end
+
+  def create
+    redirect_to(new_venue_path) and return unless request.post?
+    h = params[:venue] || {}
+
+    @venue = current_user.foursquare.addvenue h
+    unless @venue.nil?
+      current_user.foursquare.checkin(:vid => @venue["id"])
+      flash[:notice] = "You added and checked in to #{@venue["name"]}."
+      redirect_to(history_path) and return
+    else
+      flash[:error] = "Could not add #{params[:venue][:name]}"
+      redirect_to(new_venue_path) and return
+    end
+  end
+
 end
