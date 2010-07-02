1fa96b6fee4d95d9ffcd41e82741dcb63ec87fff
Ian Sefferman <iseff@iseff.com>
2010-07-02 06:25:17 +0000

    use the new foursquare mobile oauth page, and update the code for a bugfix from foursquare with redirecting back to oauth_callback.. see http://groups.google.com/group/foursquare-api/browse_thread/thread/3156ba93c2053a21/a9000fa6c7256f86?lnk=gst&q=oauth+failing#a9000fa6c7256f86 for more info on bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def oauth_authorize
-    @request_token = oauth.request_token
+    o = oauth
+    o.set_callback_url(oauth_callback_url(:host => "fortysquires.com"))
+    o.consumer.options[:authorize_path] = "/mobile/oauth/authorize"
+    @request_token = o.request_token
     session[:request_token] = @request_token.token
     session[:request_secret] = @request_token.secret
     redirect_to @request_token.authorize_url
@@ -9,8 +12,8 @@ class UsersController < ApplicationController
 
   def oauth_callback
     begin
-      logger.info "request_token: #{session[:request_token].inspect}, secret: #{session[:request_secret].inspect}, token: #{params[:oauth_token].inspect}"
-      @user = User.new_from_access_token(*oauth.authorize_from_request(session[:request_token], session[:request_secret], params[:oauth_token]))
+      logger.info "request_token: #{session[:request_token].inspect}, secret: #{session[:request_secret].inspect}, token: #{params[:oauth_token].inspect}, verifier: #{params[:oauth_verifier].inspect}"
+      @user = User.new_from_access_token(*oauth.authorize_from_request(session[:request_token], session[:request_secret], params[:oauth_verifier]))
     rescue
       flash[:error] = "Sorry, we were unable to authenticate you with Foursquare. Please try again."
       redirect_to(login_path) and return
