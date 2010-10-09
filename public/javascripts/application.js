a413108ab883ef2ce16c3d18f3c01b1acac852eb
Ian Sefferman <iseff@iseff.com>
2010-10-09 15:55:44 +0000

    make our gps work with android 2.2

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,58 @@
-    $('document').ready(function() {
-      navigator.geolocation.getCurrentPosition(
-        function(pos) {
-          lat  = pos.coords.latitude;
-          long = pos.coords.longitude;
-	  if ($('#venue_search_lat').length) {
-	    $('#venue_search_lat').val(lat);
-	  }
 
-	  if ($('#venue_search_long').length) {
-	    $('#venue_search_long').val(long);
-	  }
-       }
-     );
-   });
+function findVenues() {
+  $('#venues').html("Loading locations...");
+
+  if (gps_capable()) {
+    getLocation(
+      function(coords) {
+        var lat = coords.latitude;
+        var long = coords.longitude;
+        $.get('/checkin/nearby_venues?lat=' + lat + '&long=' + long,
+              '',
+              function(data, status, req) {
+                $('#venues').html(data);
+              }
+        );
+      }, function() {
+        $('#venues').html("Sorry, we were unable to get your location. <a href='javascript:findVenutes();'>Retry.</a>");
+      }
+    );
+  } else {
+    $('#venues').html("Sorry, your browser does not appear to support geolocation.");
+  }
+}
+
+function gps_capable() {
+  var _locator_object;
+  try {
+    _locator_object = navigator.geolocation;
+  } catch (e) {
+    return false;
+  }
+
+  if (_locator_object) return true; else return false;
+}
+
+function getLocation(successCallback, errorCallback) {
+  successCallback = successCallback || function(){};
+  errorCallback = errorCallback || function(){};
+
+  var geolocation = navigator.geolocation;
+
+  if (geolocation) {
+    try {
+      function handleSuccess(position) {
+	successCallback(position.coords);
+      }
+
+      geolocation.getCurrentPosition(handleSuccess, errorCallback, {
+	enableHighAccuracy: true,
+	maximumAge: 5000 // 5 sec.
+      });
+    } catch (err) {
+      errorCallback();
+    }
+  } else {
+    errorCallback();
+  }
+}
